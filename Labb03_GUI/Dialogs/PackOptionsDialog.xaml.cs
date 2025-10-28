8bfe74e5000fbe652d275cd807d063d724612a3d
bjornpetersson1 <bjornpetersson1@live.se>
2025-10-28 12:30:06 +0100

    Add project files.

---
+++ Labb03_GUI/Dialogs/PackOptionsDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Labb03_GUI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for PackOptionsDialog.xaml
+    /// </summary>
+    public partial class PackOptionsDialog : Window
+    {
+        public PackOptionsDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mySecondsLabel.Content = $"{mySlider.Value} seconds";
+        }
+    }
+}
