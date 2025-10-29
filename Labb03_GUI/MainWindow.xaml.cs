6d8cb9e160c21584fd80446df00eebeb1aa155eb
bjornpetersson1 <bjornpetersson1@live.se>
2025-10-29 16:01:28 +0100

    Fredriks uppstartsfix

--- Labb03_GUI/MainWindow.xaml.cs
+++ Labb03_GUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Labb03_GUI.Models;
+using Labb03_GUI.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,19 @@ namespace Labb03_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int count = 1;
         public MainWindow()
         {
             InitializeComponent();
+            var pack = new QuestionPack("MyPack");
+            DataContext = new QuestionPackViewModel(pack);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QuestionPackViewModel viewModel = (DataContext as QuestionPackViewModel);
+            viewModel.Name = "New name";
+            viewModel.Questions.Add(new Question($"{count++}VAd är gult?", "1", "2", "3", "4"));
         }
     }
 }
