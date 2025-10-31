c6a8fa76198dfd998bfc56ebfbda738ad8517435
bjornpetersson1 <bjornpetersson1@live.se>
2025-10-31 12:24:53 +0100

    Commandsnack

--- Labb03_GUI/ViewModels/PlayerViewModel.cs
+++ Labb03_GUI/ViewModels/PlayerViewModel.cs
@@ -1,13 +1,42 @@
-﻿namespace Labb03_GUI.ViewModels
+﻿using Labb03_GUI.Command;
+
+namespace Labb03_GUI.ViewModels
 {
     class PlayerViewModel : ViewModelBase
     {
         private readonly MainWindowViewModel? _mainWindowViewModel;
+
+        public DelegateCommand SetPackNameCommand { get; }
         public QuestionPackViewModel? ActivePack { get => _mainWindowViewModel?.ActivePack; }
 
         public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
         {
             this._mainWindowViewModel = mainWindowViewModel;
+
+            SetPackNameCommand = new DelegateCommand(SetPackName, CanSetPackName);
+            DemoText = string.Empty;
+        }
+        private string _demoText;
+
+        public string DemoText
+        {
+            get { return _demoText; }
+            set { 
+                _demoText = value;
+                RaisePropertyChanged();
+                SetPackNameCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+
+        private bool CanSetPackName(object? arg)
+        {
+            return DemoText.Length > 0;
+        }
+
+        private void SetPackName(object? obj)
+        {
+            ActivePack.Name = DemoText;
         }
     }
 }
